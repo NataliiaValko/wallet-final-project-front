8883ca04ed33981201dd1e8c38639f9be99e01cf
Mike-Max <scartman1992@gmail.com>
2021-12-08 11:33:44 +0000

    more two

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import LoginForm from '../../components/LoginForm';
+import LoginForm from 'components/LoginForm';
 
 const Login = () => {
   <h1>Login page</h1>;
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,4 +1,4 @@
-import RegisterForm from '../../components/registrationForm';
+import RegisterForm from 'components/registrationForm';
 
 const Registration = () => {
   <h1>Registration page</h1>;
